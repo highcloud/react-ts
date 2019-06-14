ed366f5bbe0cbb0df32f8bf402179e676e05cb5f
Jiping <WangJiping@gmail.com>
2019-06-13 23:24:44 -0400

    todo: custom div attributes?

--- src/common/CustomerChat.tsx
+++ src/common/CustomerChat.tsx
@@ -10,8 +10,9 @@ import { ConfigContext } from '../hooks';
 
 // https://developers.facebook.com/docs/messenger-platform/discovery/customer-chat-plugin
 class CustomerChat extends React.PureComponent {
+  timeout: number = 0;
   componentDidMount() {
-    this.timeout = setTimeout(() => {
+    this.timeout = window.setTimeout(() => { //todo: how to void windows.setTimeout vs NodeJS.setTimeout
       fb(FB => this.timeout && FB.XFBML.parse());
     }, 3000);
   }
@@ -25,17 +26,18 @@ class CustomerChat extends React.PureComponent {
     return (
       <ConfigContext.Consumer>
         {config => (
+          //@ts-ignore //todo: how to expand div attributes?
           <div
             className="fb-customerchat"
             attribution="setup_tool"
             page_id={config.facebook.pageId}
-            // theme_color="..."
-            // logged_in_greeting="..."
-            // logged_out_greeting="..."
-            // greeting_dialog_display="..."
-            // greeting_dialog_delay="..."
-            // minimized="false"
-            // ref="..."
+          // theme_color="..."
+          // logged_in_greeting="..."
+          // logged_out_greeting="..."
+          // greeting_dialog_display="..."
+          // greeting_dialog_delay="..."
+          // minimized="false"
+          // ref="..."
           />
         )}
       </ConfigContext.Consumer>
