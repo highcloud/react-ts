ef8c7850ffd62c0d10e8c629099bf92213b674c0
Jiping <WangJiping@gmail.com>
2019-06-16 22:18:57 -0400

    explicit global scope

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,21 +1,22 @@
-interface Window {
-  dataLayer: [];
-  data: any;
-  config: any;
-  FB: fb.FacebookStatic;
-  fbAsyncInit: () => any;
-}
-
-interface NodeModule {
-  hot: any;
-}
-declare var module: NodeModule
-
-function nullCheck<T>(val: T) {
-  if (!val) {
-    const msg = `expect type: ${typeof T}, but got: ${val} `
-    console.log(msg)
-    throw new Error(msg)
+declare global {
+  interface Window {
+    dataLayer: [];
+    data: any;
+    config: any;
+    FB: fb.FacebookStatic;
+    fbAsyncInit: () => any;
   }
 
+  interface NodeModule {
+    hot: any;
+  }
+  declare var module: NodeModule
+
+  function nullCheck<T>(val: T) {
+    if (!val) {
+      const msg = `expect type: ${typeof T}, but got: ${val} `
+      console.log(msg)
+      throw new Error(msg)
+    }
+  }
 }
