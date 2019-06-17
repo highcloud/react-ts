35f793723e8ad27275fd295f2f974ca1cbf224c9
Jiping <WangJiping@gmail.com>
2019-06-17 14:14:13 -0400

    load-script declaration

--- src/types/load-script.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "load-script" {
-    import { ScriptHTMLAttributes } from "react";
-    declare function load(str: string, opts: ScriptHTMLAttributes | loadScriptCallback);
-    declare function loadScriptCallback(err?: Error, script: ScriptHTMLAttributes): void;
-    export = load
-}
