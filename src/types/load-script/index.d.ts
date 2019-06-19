0a4a4b716824259de31df3a12a555453824d5c20
Jiping <WangJiping@gmail.com>
2019-06-19 12:04:57 -0400

    clean

--- src/types/load-script/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "load-script" {
-    type loadScriptCallback = (err?: Error, script?: HTMLScriptElement) => void
-    function load(str: string, opts: HTMLScriptElement | loadScriptCallback): void;
-    export = load
-}
