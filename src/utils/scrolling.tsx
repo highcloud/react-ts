0df890cde866c46bf5799e4a56501e6974cf07eb
Jiping <WangJiping@gmail.com>
2019-06-27 18:35:58 -0400

    scrolling

--- src/utils/scrolling.tsx
+++ src/utils/scrolling.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 /**
  * React Starter Kit for Firebase
  * https://github.com/kriasoft/react-firebase-starter
@@ -7,21 +8,24 @@
 /* @flow */
 
 import { canUseDOM } from './env';
+import { History, LocationKey } from 'history';
 
-const listeners = new Set();
-const scrollPositions = new Map();
+type Callback = (pos: number) => void
+
+const listeners = new Set<Callback>();
+const scrollPositions = new Map<LocationKey, number>();
 
 let last_known_scroll_position = 0;
 let ticking = false;
 
-let history;
+let history: History;
 
 if (canUseDOM) {
   window.addEventListener('scroll', () => {
     last_known_scroll_position = window.scrollY;
     if (!ticking) {
       window.requestAnimationFrame(() => {
-        if (history) {
+        if (history && history.location && history.location.key) {
           scrollPositions.set(history.location.key, last_known_scroll_position);
         }
         listeners.forEach(cb => cb(last_known_scroll_position));
@@ -32,15 +36,17 @@ if (canUseDOM) {
   });
 }
 
-export function onScroll(cb) {
+export function onScroll(cb: Callback) {
   listeners.add(cb);
   return () => listeners.delete(cb);
 }
 
-export function setHistory(browserHistory) {
+export function setHistory(browserHistory: History) {
   history = browserHistory;
 }
 
-export function getScrollPosition(locationKey) {
+export function getScrollPosition(locationKey?: LocationKey) {
+  if (!locationKey) return undefined
+
   return scrollPositions.get(locationKey);
 }
