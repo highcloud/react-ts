1f03d862edecba004dce00f81903c6a1f8ac0202
Jiping <WangJiping@gmail.com>
2019-06-28 00:17:15 -0400

    bug?

--- src/news/Story.tsx
+++ src/news/Story.tsx
@@ -1,26 +1,33 @@
 /**
- * React Starter Kit for Firebase
- * https://github.com/kriasoft/react-firebase-starter
- * Copyright (c) 2015-present Kriasoft | MIT License
- */
+* React Starter Kit for Firebase
+* https://github.com/kriasoft/react-firebase-starter
+* Copyright (c) 2015-present Kriasoft | MIT License
+*/
 
 /* @flow */
 
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
 import { graphql, createFragmentContainer } from 'react-relay';
 
 import Link from '../common/Link';
+import { StyleRulesCallback, Theme, withStyles, WithStyles } from '@material-ui/core';
 
-const styles = theme => ({
+const styles: StyleRulesCallback = (theme: Theme) => ({
   root: {
     ...theme.mixins.content,
   },
 });
-
-function Story({ classes: s, data: { title, text, isURL } }) {
+interface Props extends WithStyles {
+  data: StoryData,
+}
+interface StoryData {
+  title: string, text: string, isURL: boolean,
+}
+function Story(props: Props) {
+  const { classes: s, } = props
+  const { title, text, isURL } = props.data
   return (
     <div className={s.root}>
       <Typography variant="h3" gutterBottom>
@@ -31,12 +38,12 @@ function Story({ classes: s, data: { title, text, isURL } }) {
           <a href={text}>{text}</a>
         </Typography>
       ) : (
-        text &&
-        text.split('\n').map(x => <Typography gutterBottom>{x}</Typography>)
-      )}
+          text &&
+          text.split('\n').map(x => <Typography gutterBottom>{x}</Typography>)
+        )}
       <div style={{ marginTop: 10, textAlign: 'right' }}>
-        <Button component={Link} href="/news">
-          Go back
+        <Button href="/news"> {/*bug ?*/}
+          <Link>Go back</Link>
         </Button>
       </div>
     </div>
